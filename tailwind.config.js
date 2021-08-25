df007910eddc645c79c80b710992041627b0aee1
NickLanger1990 <info@nicklanger.de>
2021-08-25 23:51:34 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    purge: {
+        content: [
+            "./src/*.html"
+        ],
+        options: {},
+    },
+    mode: "jit",
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
