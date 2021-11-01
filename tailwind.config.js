4efb3ac58c1ab93a2f954e551a8c2f58c6acbca4
bempe@bennebensen.de <bempe@bennebensen.de>
2021-11-01 14:34:45 +0100

    St. Oberholz

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,16 @@ module.exports = {
     mode: "jit",
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'primary': '#156DB5',
+                },
+        
+            fontFamily: {
+                'overpass': ['overpass', 'system-ui'],
+                'ibm': ['IBM Plex Sans', 'system-ui']
+            },
+        },
     },
     variants: {
         extend: {},
